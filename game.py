94d839bd4394248971dc2880979053efc99a29ea
Gustavo P <guprado.silva@gmail.com>
2023-08-22 15:55:48 -0300

    [fix] 3D raycast done and working, no collisions yet

--- game.py
+++ game.py
@@ -3,49 +3,74 @@
 import pygame, math
 from pygame.locals import *
 
-WIDTH  = 1280
-HEIGHT = 720
+WIDTH  = 1024
+HEIGHT = 512
 RED = (250, 0, 0)
+DARKRED = (150, 0, 0)
 SCALE = 1
 OLDSCALE = SCALE
-PLAYER_SIZE = 25 * SCALE / 1.1
+PLAYER_SIZE = 8
 
 DR = 0.0174533 # um grau em radianos
-
-mapOffset = 0
-
 def dist(x1, y1, x2, y2, ang):
    return math.sqrt( ((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)) )
 
 def drawRays2D():
-   ######################### Checa horizontal ###############################
-   ra = player.a - DR
+   ra = player.a + math.radians(30)
    if ra < 0:           ra += 2*math.pi
    if ra > 2*math.pi:   ra -= 2*math.pi
    px = player.rect.centerx
    py = player.rect.centery
 
    for r in range(60):
+      ######################### Checa vertical ###############################
+      dof = 0; distV=1000000
+      ntan = math.tan(ra)
+      
+      # Raio para esquerda
+      if math.cos(ra) > 0.001:
+         rx = (int(px >> 6)<<6)+64
+         ry = (px - rx) * ntan + py
+         xo = 64; yo = -xo*ntan
+
+      # Raio para direita
+      elif math.cos(ra) < -0.001:
+         rx = (int(px >> 6)<<6)-0.0001
+         ry = (px - rx) * ntan + py
+         xo = -64; yo = -xo*ntan
+
+      # Raio reto para cima ou baixo
+      else: rx = px; ry = py; dof = 8
+      
+      while (dof < 8):
+         mx = int(rx) >> 6; my = int(ry) >> 6
+         mp = my * mapX + mx
+
+         # bateu na parede
+         if(mp > 0 and mp < mapX*mapY and map[mp] == 1):
+            distV=math.cos(ra)*(rx-px)-math.sin(ra)*(ry-py); dof = 8
+         else: rx += xo; ry += yo; dof += 1
+      vx=rx; vy=ry
+
+   ######################### Checa horizontal ###############################
       # Checa linhas horizontais
-      distH=1000000; hx = px; hy = py
-      dof = 0
-      atan = -1/math.tan(ra)
+      distH=1000000; dof = 0
+      atan = 1/ntan
 
       # Raio para cima
-      if ra > math.pi and ra < 2*math.pi:
+      if math.sin(ra) > 0.001:
          ry = (int(py >> 6)<<6)-0.0001
          rx = (py - ry) * atan + px
          yo = -64; xo = -yo*atan
 
       # Raio para baixo
-      if ra < math.pi and ra > 0:
+      elif math.sin(ra) < -0.001:
          ry = (int(py >> 6)<<6)+64
          rx = (py - ry) * atan + px
          yo = 64; xo = -yo*atan
 
       # Raio reto para esquerda ou direita
-      if (ra == 0 or ra == 2*math.pi):
-         rx = px; ry = py; dof = 8
+      else: rx = px; ry = py; dof = 8
       
       while (dof < 8):
          mx = int(rx) >> 6; my = int(ry) >> 6
@@ -53,60 +78,52 @@ def drawRays2D():
 
          # bateu na parede
          if(mp > 0 and mp < mapX*mapY and map[mp] == 1):
-            hx = rx; hy = ry; distH=dist(px, py, hx, hy, ra); dof = 8
-         else:
-            rx += xo; ry += yo; dof += 1
-
-      ######################### Checa vertical ###############################
-      dof = 0
-      ntan = -math.tan(ra)
-      distV=1000000; vx = px; vy = py
-
-      # Raio para esquerda
-      if ra > math.pi/2 and ra < 3*math.pi/2:
-         rx = (int(px >> 6)<<6)-0.0001
-         ry = (px - rx) * ntan + py
-         xo = -64; yo = -xo*ntan
-
-      # Raio para direita
-      if ra < math.pi/2 or ra > 3*math.pi/2:
-         rx = (int(px >> 6)<<6)+64
-         ry = (px - rx) * ntan + py
-         xo = 64; yo = -xo*ntan
-
-      # Raio reto para cima ou baixo
-      if (ra == math.pi/2 or ra == 3*math.pi/2):
-         rx = px; ry = py; dof = 8
-      
-      while (dof < 8):
-         mx = int(rx) >> 6; my = int(ry) >> 6
-         mp = my * mapX + mx
-
-         # bateu na parede
-         if(mp > 0 and mp < mapX*mapY and map[mp] == 1):
-            vx = rx; vy = ry; distV=dist(px, py, vx, vy, ra); dof = 8
-         else:
-            rx += xo; ry += yo; dof += 1
+            distH=math.cos(ra)*(rx-px)-math.sin(ra)*(ry-py); dof = 8
+         else: rx += xo; ry += yo; dof += 1
 
       # Desenha rays
-      if distV < distH: rx = vx; ry = vy
-      if distV > distH: rx = hx; ry = hy
+      color = DARKRED
+      if distV < distH: rx = vx; ry = vy; distH=distV; color = RED
       
-      pygame.draw.line(screen, (22, 255, 53), (px, py), (rx, ry), 2)
+      if mapOn: pygame.draw.line(screen, (22, 255, 53), (px, py), (rx, ry), 2)
+
+      ######################### Desenha 3D ###############################
+      # Fix fisheye
+      ca = player.a - ra
+      if ca < 0:           ca += 2*math.pi
+      if ca > 2*math.pi:   ca -= 2*math.pi
+      distH*=math.cos(ca)
+
+      lineH = (mapS*320)/distH
+      if lineH > 320: lineH = 320
+
+      lineO=160-lineH/2
+
+      pygame.draw.line(screen, color, (8*r+530,lineO), (8*r+530,lineH+lineO), 8)
 
       # Aumenta angulo do proximo ray
-      ra += DR
+      ra -= DR;
       if ra < 0:           ra += 2*math.pi
       if ra > 2*math.pi:   ra -= 2*math.pi
+
+   #Desenha linha com a direcao do player
+   cx = player.rect.centerx; cy = player.rect.centery
+   if mapOn: 
+      pygame.draw.line(
+         screen, (RED),
+         (cx, cy),
+         (cx + player.dx * 20, cy - player.dy * 20),
+         2
+      )
 
 def xToTile(x, y, s):
    # Calcula indice da tile basedo no x e y do player e em uma escala
    mapScaling = 64 * s
-   return ((x - mapScaling/2)/(mapScaling + mapOffset), (y - mapScaling/2)/(mapScaling + mapOffset))
+   return ((x - mapScaling/2)/(mapScaling), (y - mapScaling/2)/(mapScaling))
 
 def tileToX(x, y):
    # Calcula posicao x e y baseado no indice da tile
-   xo = x * mapS + mapOffset; yo = y * mapS + mapOffset
+   xo = x * mapS; yo = y * mapS
    return (xo + mapS/2, yo + mapS/2)
 
 class Player():
@@ -117,32 +134,31 @@ class Player():
       self.surf = pygame.Surface((PLAYER_SIZE, PLAYER_SIZE))
       self.surf.fill(RED)
       self.rect = self.surf.get_rect()
-      # self.rect.center = tileToX(mapX-2, mapY-2)
-      self.rect.center = tileToX(2, 6)
+      self.rect.center = tileToX(mapX-2, mapY-2)
 
       # Direcao inicial
-      self.a  = 7*math.pi/4
-      self.dx = math.cos(self.a) * 5; self.dy = math.sin(self.a) * 5
+      self.a  = math.pi/2
+      self.dx = math.cos(self.a); self.dy = math.sin(self.a)
 
    def update(self, pressed_keys):
       # Se move de acordo com as teclas
       if pressed_keys[K_w]:
-         self.rect.move_ip(player.dx, player.dy)
+         self.rect.move_ip(player.dx*5, -player.dy*5)
 
       if pressed_keys[K_s]:
-         self.rect.move_ip(-player.dx, -player.dy)
+         self.rect.move_ip(-player.dx*5, player.dy*5)
 
       if pressed_keys[K_a]:
-         self.a -= 0.1
-         if (self.a < 0.1):           self.a += 2 * math.pi
+         self.a += 0.1
+         if (self.a < 0.1):       self.a += 2 * math.pi
          if (self.a > 2*math.pi): self.a -= 2 * math.pi
-         self.dx = math.cos(self.a) * 5; self.dy = math.sin(self.a) * 5
+         self.dx = math.cos(self.a); self.dy = math.sin(self.a)
                            
       if pressed_keys[K_d]:
-         self.a += 0.1
-         if (self.a < 0.1):           self.a += 2 * math.pi
+         self.a -= 0.1
+         if (self.a < 0.1):       self.a += 2 * math.pi
          if (self.a > 2*math.pi): self.a -= 2 * math.pi
-         self.dx = math.cos(self.a) * 5; self.dy = math.sin(self.a) * 5
+         self.dx = math.cos(self.a); self.dy = math.sin(self.a)
 
       # Mantem player na tela
       if self.rect.left < 0:
@@ -175,7 +191,7 @@ def drawMap2D():
             color = (0, 0, 0)
             
          # Offset do mapa em relacao ao canto sup esquerdo
-         xo = x * mapS + mapOffset; yo = y * mapS + mapOffset
+         xo = x * mapS; yo = y * mapS
 
          # Desenha parede no minimapa
          wallSurf = pygame.Surface((mapS, mapS))
@@ -188,16 +204,6 @@ def drawMap2D():
             pygame.draw.line( screen, ((138, 138, 138)), (xo, yo), (xo + mapS, yo), 1)
             pygame.draw.line( screen, ((138, 138, 138)), (xo, yo + mapS), (xo + mapS, yo + mapS), 1)
             pygame.draw.line( screen, ((138, 138, 138)), (xo + mapS, yo), (xo + mapS, yo + mapS), 1)
-
-   # Desenha linha com a direcao do player
-   # cx = player.rect.centerx
-   # cy = player.rect.centery
-   # pygame.draw.line(
-   #    screen, (RED),
-   #    (cx, cy),
-   #    (cx + player.dx * PLAYER_SIZE / 2, cy + player.dy * PLAYER_SIZE / 2),
-   #    2
-   # )
 
 # Opcoes booleanas
 done = False
@@ -248,18 +254,8 @@ while not done:
    screen.fill((105, 105, 105))
    if mapOn:
       drawMap2D()
-
-      # Reescala player recolocando ele na tile onde estava, mas com x e y escalados
-      if rescale:
-         oldCenter = xToTile(player.rect.centerx, player.rect.centery, OLDSCALE)
-
-         player.surf = pygame.transform.scale(player.surf, (PLAYER_SIZE, PLAYER_SIZE))
-         player.rect = player.surf.get_rect()
-         player.rect.center = tileToX(oldCenter[0], oldCenter[1])
-         
-         rescale = False
       screen.blit(player.surf, player.rect)
-      drawRays2D()
+   drawRays2D()
 
    # Flipa display e espera relogio
    pygame.display.flip()
